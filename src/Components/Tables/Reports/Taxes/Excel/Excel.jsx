22b5bacad341fec691d1a5083c21fe6d09112b74
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2023-03-06 16:09:19 -0300

    Update (../Excel) update export format

--- src/Components/Tables/Reports/Taxes/Excel/Excel.jsx
+++ src/Components/Tables/Reports/Taxes/Excel/Excel.jsx
@@ -6,33 +6,19 @@ const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
 const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
 
 export default function Excel({ taxes }) {
+  const data = [];
 
-    const data = [];
-
-    return (
+  return (
     <ExcelFile element={<button className="btn btn-primary">Exportar</button>}>
       <ExcelSheet data={data} name="Employees">
-        <ExcelColumn label="Fecha" value="VEN_FECHA" />
-        <ExcelColumn label="Comprobante" value="COMPROBANTE" />
-        <ExcelColumn label="Est." value="VEN_ESTABLECIMIENTO" />
-        <ExcelColumn label="Pto." value="VEN_PTOEMISION" />
-        <ExcelColumn label="Nro" value="VEN_NUMERO" />
-        <ExcelColumn label="RUC/Ced/Pas" value="CLI_IDENTIFICACION" />
-        <ExcelColumn label="Cliente" value="CLI_NOMBRE" />
-        <ExcelColumn label="Sub. 0" value="VEN_SUBTOTAL0" />
-        <ExcelColumn label="Sub IVA" value="VEN_SUBTOTAL12" />
-        <ExcelColumn label="Descuento" value="VEN_DESCUENTO" />
-        <ExcelColumn label="Subtotal" value="VEN_SUBTOTAL" />
-        <ExcelColumn label="IVA" value="IVA" />
-        <ExcelColumn label="Total" value="VEN_TOTAL" />
-        <ExcelColumn label="FP" value="VEN_FPAGO" />
-        <ExcelColumn label="Estado" value="VEN_ESTADO" />
-
-        <ExcelColumn label="Codigo Item" value="ITE_CODIGO" />
-        <ExcelColumn label="Cantidad" value="VED_CANTIDAD" />
-        <ExcelColumn label="Descripcion" value="ITE_DESCRIPCION" />
-        <ExcelColumn label="Precio" value="VED_PUNITARIO" />
-        <ExcelColumn label="Valor" value="VALOR" />
+        <ExcelColumn label="Total Sales and Shipping" value="null" />
+        <ExcelColumn label="Total Expenses" value="null" />
+        <ExcelColumn label="Total Profit" value="null" />
+      </ExcelSheet>
+      <ExcelSheet data={data} name="Employees">
+        <ExcelColumn label="Month" value="null" />
+        <ExcelColumn label="Sales / Shipping" value="null" />
+        <ExcelColumn label="Expenses" value="null" />
       </ExcelSheet>
     </ExcelFile>
   );
